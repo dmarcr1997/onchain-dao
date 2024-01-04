853907528735d695f8db700694758af4cc6abc99
Damon Marc Rocha II <dmarcr1997@gmail.com>
2024-01-03 20:49:31 -0600

    DAO Contract

---
+++ contracts/CryptoNautsDAO.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * Interface for the MockNFTMarketplace
+ */
+interface IMockNFTMarketplace {
+    function getPrice() external view returns (uint256);
+
+    function available(uint256 _tokenId) external view returns (bool);
+
+    function purchase(uint256 _tokenId) external payable;
+}
+
+/**
+ * Minimal interface for CryptoNautsNFT containing only two functions
+ * that we are interested in
+ */
+interface ICryptoNautsNFT {
+    function balanceOf(address owner) external view returns (uint256);
+
+    function tokenOfOwnerByIndex(
+        address owner,
+        uint256 index
+    ) external view returns (uint256);
+}
+
+contract CryptoDevsDAO is Ownable {
+    struct Proposal {
+        uint256 nftTokenId;
+        uint256 deadline;
+        uint256 yayVotes;
+        uint256 nayVotes;
+        bool executed;
+        mapping(uint256 => bool) voters;
+    }
+
+    enum Vote {
+        YAY,
+        NAY
+    }
+
+    modifier nftHolderOnly() {
+        require(cryptoNautsNFT.balanceOf(msg.sender) > 0, "NOT A MEMBER");
+        _;
+    }
+
+    modifier activeProposalOnly(uint256 indx) {
+        require(proposals[indx].deadline > block.timestamp, "VOTE_ENDED");
+        _;
+    }
+
+    modifier inactiveProposalOnly(uint256 proposalIndex) {
+        require(
+            proposals[proposalIndex].deadline <= block.timestamp,
+            "DEADLINE_NOT_EXCEEDED"
+        );
+        require(
+            proposals[proposalIndex].executed == false,
+            "PROPOSAL_ALREADY_EXECUTED"
+        );
+        _;
+    }
+
+    mapping(uint256 => address) public proposals;
+    uint256 public numProposals;
+
+    IMockNFTMarketplace nftMarketplace;
+    ICryptoNautsNFT cryptoNautsNFT;
+
+    constructor(address _nftMarketplace, address _cryptoNautsNFT) payable {
+        nftMarketplace = IMockNFTMarketplace(_nftMarketplace);
+        cryptoNautsNFT = ICryptoNautsNFT(_cryptoNautsNFT);
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+
+    function withdrawEther() external onlyOwner {
+        uint256 amount = address(this).balance;
+        require(amount > 0, "Nothing to withdraw");
+        (bool sent, ) = payable(owner()).call{value: amount}("");
+        require(sent, "FAILED_TO_WITHDRAW_ETHER");
+    }
+
+    function createProposal(
+        uint256 _nftTokenId
+    ) external nftHolderOnly returns (uint256) {
+        require(nftMarketplace.available(_nftTokenId), "NFT_NOT_FOR_SALE");
+        Proposal storage proposal = proposals[numProposals];
+        proposal.nftTokenId = _nftTokenId;
+        // Set the proposal's voting deadline to be (current time + 5 minutes)
+        proposal.deadline = block.timestamp + 5 minutes;
+
+        numProposals++;
+
+        return numProposals - 1;
+    }
+
+    function voteOnProposal(
+        uint256 proposalIndex,
+        Vote vote
+    ) external nftHolderOnly activeProposalOnly(proposalIndex) {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        uint256 voterNFTBalance = cryptoNautsNFT.balanceOf(msg.sender);
+        uint256 numVotes = 0;
+
+        for (uint256 i = 0; i < voterNFTBalance; i++) {
+            uint256 tokenId = cryptoNautsNFT.tokenOfOwnerByIndex(msg.sender, i);
+            if (proposal.voters[tokenId] == false) {
+                numVotes++;
+                proposal.voters[tokenId] = true;
+            }
+        }
+        require(numVotes > 0, "ALREADY_VOTED");
+
+        if (vote == Vote.YAY) {
+            proposal.yayVotes += numVotes;
+        } else {
+            proposal.nayVotes += numVotes;
+        }
+    }
+
+    function executeProposal(
+        uint256 proposalIndex
+    ) external nftHolderOnly inactiveProposalOnly(proposalIndex) {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        if (proposal.yayVotes > proposal.nayVotes) {
+            uint256 nftPrice = nftMarketplace.getPrice();
+            require(address(this).balance >= nftPrice, "NOT_ENOUGH_FUNDS");
+            nftMarketplace.purchase{value: nftPrice}(proposal.nftTokenId);
+        }
+        proposal.executed = true;
+    }
+}
--- contracts/CryptoNautsNFT.sol
+++ contracts/CryptoNautsNFT.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract CryptoNauts is ERC721Enumerable {
+contract CryptoNautsNFT is ERC721Enumerable {
     // Initialize the ERC-721 contract
     constructor() ERC721("CryptoNauts", "CN") {}
 
